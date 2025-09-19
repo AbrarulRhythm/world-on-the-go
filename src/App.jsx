b1d1ebf1e4f243264363f99e3ceb9292b17e1602
Abrarul Rhythm <[....email>
2025-09-20 00:17:06 +0600

    fetch countries data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { Suspense } from 'react';
 import './App.css';
+import Countries from './components/Countries/Countries';
+
+const fetchCountries = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/all');
+  return res.json();
+}
 
 function App() {
+
+  const countriesPromise = fetchCountries();
+
   return (
     <>
-
+      <Suspense fallback={<p>Country Data Loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
